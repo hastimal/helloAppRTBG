c8344ff77b82f3d24e125b2948401f6a8d863a85
Jangid <hjdk4@kc-sce463lab-10.kc.umkc.edu>
2015-09-01 17:04:07 -0500

    Initial Commit

---
+++ helloAppRTBG/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  helloAppRTBG
+//
+//  Created by Jangid, Hastimal (UMKC-Student) on 9/1/15.
+//  Copyright (c) 2015 UMKC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
